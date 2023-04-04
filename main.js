b932065188bd26209cd3da1059cc0a1099fc62a5
priyanshi0707 <89638179+priyanshi0707@users.noreply.github.com>
2023-04-04 14:15:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song="";
+function preload(){
+    song=loadSound("music.mp3");
+    
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function play(){
+    song.play();
+song.setVolume(1);
+    song.rate(1);
+}
